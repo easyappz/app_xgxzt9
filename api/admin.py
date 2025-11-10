678bce54e7a7b13904a0cff98815b0f51389feb2
IbragimKhaniev <newinterny@gmail.com>
2025-11-10 18:12:10 +0300

    feat: add Member JWT authentication backend

--- api/admin.py
+++ api/admin.py
@@ -4,17 +4,21 @@ from .models import Member
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ["id", "email", "first_name", "last_name", "created_at"]
-    list_filter = ["created_at"]
-    search_fields = ["email", "first_name", "last_name"]
-    readonly_fields = ["password", "created_at", "updated_at"]
-    ordering = ["-created_at"]
+    list_display = ['id', 'email', 'first_name', 'last_name', 'created_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['email', 'first_name', 'last_name']
+    readonly_fields = ['created_at', 'updated_at', 'password']
+    ordering = ['-created_at']
     
     fieldsets = (
-        ("Основная информация", {
-            "fields": ("email", "first_name", "last_name")
+        ('Основная информация', {
+            'fields': ('email', 'first_name', 'last_name')
         }),
-        ("Системная информация", {
-            "fields": ("password", "created_at", "updated_at")
+        ('Безопасность', {
+            'fields': ('password',)
+        }),
+        ('Системная информация', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
         }),
     )
--- api/authentication.py
+++ api/authentication.py
@@ -4,12 +4,16 @@ from .models import Member
 
 
 class MemberJWTAuthentication(JWTAuthentication):
+    """
+    Custom JWT authentication that uses Member model instead of Django User model.
+    """
     def get_user(self, validated_token):
         try:
-            member_id = validated_token.get("member_id")
+            member_id = validated_token.get('member_id')
             if member_id is None:
-                raise InvalidToken("Token does not contain member_id")
+                raise InvalidToken('Token does not contain member_id')
+            
             member = Member.objects.get(id=member_id)
             return member
         except Member.DoesNotExist:
-            raise InvalidToken("Member not found")
+            raise InvalidToken('Member not found')
